70dd429754db2c84801ba5eeb36864ce05ddc995
jstnliu <jlcheeseburger@gmail.com>
2023-11-27 11:25:59 -0800

    MERN-stack infra toggle auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react'
 import LoginForm from "../../components/LoginForm/LoginForm"
 import SignUpForm from "../../components/SignUpForm/SignUpForm"
 
 export default function AuthPage({ setUser }) {
+
+const [showLoginForm, setShowLoginForm] = useState(true)
+
+const toggleForm = () => {
+  setShowLoginForm(prevShowLoginForm => !prevShowLoginForm) 
+}
+
   return (
     <>
-      <h1>AuthPage</h1> 
-      <SignUpForm setUser={ setUser } />
-      <LoginForm setUser={ setUser } />
+      <h1>AuthPage</h1>
+      <button onClick={ toggleForm }>
+        {showLoginForm ? 'Switch to Sign Up' : 'Switch to Log In'}
+      </button>
+      {showLoginForm ? (
+        <LoginForm setUser={ setUser } />
+      ) : (
+        <SignUpForm setUser={ setUser } />
+      )}
     </>
   )
 }
